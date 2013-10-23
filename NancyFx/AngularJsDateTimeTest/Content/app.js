22a334001ac80e95acaf183af383c3b3b5bb4218
rabitarochan <rabitarochan@gmail.com>
2013-10-24 01:59:22 +0900

    Create NancyFx test app.

---
+++ NancyFx/AngularJsDateTimeTest/Content/app.js
@@ -0,0 +1,12 @@
+﻿function testCtrl($scope, $http) {
+    $scope.times = [];
+    $scope.getTime = function() {
+        $http({
+            url: '/now',
+            method: 'GET'
+        }).success(function (data) {
+            console.log(data.time);
+            $scope.times.push(data);
+        });
+    };
+}
---
+++ NancyFx/AngularJsDateTimeTest/Modules/HomeModule.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Nancy;
+
+namespace AngularJsDateTimeTest.Modules
+{
+    public class HomeModule : NancyModule
+    {
+        public HomeModule()
+        {
+            Get["/"] = _ => {
+                return View["index"];
+            };
+
+            Get["/stable"] = _ => {
+                return View["stable"];
+            };
+
+            Get["/unstable"] = _ => {
+                return View["unstable"];
+            };
+
+            Get["/now"] = _ => {
+                var now = DateTime.Now;
+                return Response.AsJson(new {time = now});
+            };
+        }
+    }
+}
